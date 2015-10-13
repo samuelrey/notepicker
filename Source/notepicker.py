Fixed stereo averaging - problem may arise given channels with different signals. Fixed signal assignment - no foreseeable issues at this point. Started getFrequency tests -  problem with frequencies over 4000 given a sample rate of 44100.

Source/notepicker.py
@@ -16,6 +16,7 @@ import scipy.signal	# fftconvolve
 import matplotlib.mlab	# difference
 
 class NotePicker():
+
 	def __init__(self):		
 		# OCTAVES represents the frequency bounds of every octave
 		# BASE_FR represents the individual frequencies in the 0th octave
@@ -51,12 +52,16 @@ class NotePicker():
 				sys.exit(1)
 
 			# if there are two channels, average them.
-			elif( self.channels == 2 ):
+			if( self.channels == 2 ):
 				actual = ''
-				for (sample1, sample2) in zip(self.signal[0::2], self.signal[1::2]):
-					actual += hex((ord(sample1) + ord(sample2)) / 2)[2:].zfill(2).decode('hex')
+				indices = numpy.arange(len(self.signal))
+				for index in indices[0::4]:
+					first = hex((ord(self.signal[index]) + ord(self.signal[index + 2])) / 2)[2:].zfill(2)
+					second = hex((ord(self.signal[index + 1]) + ord(self.signal[index + 3])) / 2)[2:].zfill(2)
+					actual = actual + first.decode('hex')
+					actual = actual + second.decode('hex')
+
 				self.signal = actual
-				self.no_samples /= 2
 
 			# create numpy array to represent signal string as appropriate data type
 			# according to the sample width.
@@ -64,22 +69,8 @@ class NotePicker():
 				raise wave.Error 
 				sys.exit(1)
 
-			if( self.sample_width == 3 ):
-				array = numpy.empty((self.no_samples, self.channels, 4), dtype = numpy.uint8)
-				raw_bytes = numpy.fromstring(self.signal, dtype = numpy.uint8)
-				array[:, :, :self.sample_width] = raw_bytes.reshape(-1, self.channels, self.sample_width)
-				array[:, :, self.sample_width:] = (array[:, :, self.sample_width - 1:self.sample_width] >> 7) * 255
-				self.signal = array.view('<i4').reshape(array.shape[:-1])
-				self.signal = self.signal.reshape(-1)
-
-			else:
-				if( self.sample_width == 1 ):
-					dt_char = 'u'
-				else:
-					dt_char = 'i'
-				array = numpy.fromstring(self.signal, dtype = '<%s%d' % (dt_char, self.sample_width))
-				self.signal = array.reshape(-1)
-
+			self.signal = numpy.fromstring(self.signal, dtype = '<i2')
+				
 			wav.close()
 
 		except EOFError:
@@ -94,24 +85,25 @@ class NotePicker():
 			raise wave.Error
 			sys.exit(1)
 
-	def getFrequency(self, signal, sample_rate):
+	def getFrequency(self):
 		''' Determine the frequency of the signal. '''
 
-		# correct the signal.
-		signal = scipy.signal.fftconvolve(signal, signal[::-1], mode='full')
-		signal = signal[len(signal)/2:]
+		try:
+			# correct the signal.
+			auto = scipy.signal.fftconvolve(self.signal, self.signal[::-1], mode='full')
+			auto = auto[len(auto)/2:]
 
-		# find the first minimum point in the signal.
-		difference = numpy.diff(signal)
-		start = matplotlib.mlab.find(difference > 0)[0]
-    
-		# find the peak from that position.
-		peak = numpy.argmax(signal[start:]) + start
+			# find the first minimum point in the signal.
+			difference = numpy.diff(auto)
+			start = matplotlib.mlab.find(difference > 0)[0]
 
-		# calculate the period using parabolic interpolation.
-		period = 1/2.0 * (signal[peak-1] - signal[peak+1]) / (signal[peak-1] - 2 * signal[peak] + signal[peak+1]) + peak
-    
-		return (sample_rate / period)
+			# find the peak from that position.
+			peak = numpy.argmax(auto[start:]) + start
+			# calculate the period using parabolic interpolation.
+			period = 1/2.0 * (auto[peak-1] - auto[peak+1]) / (auto[peak-1] - 2 * auto[peak] + auto[peak+1]) + peak
+			self.frequency = self.sample_rate / period
+		except:
+			print traceback.format_exc()
 
 	def getNote(self, frequency):
 		''' Match the frequency to the musical note. '''
@@ -137,6 +129,8 @@ class NotePicker():
 if __name__ == '__main__':
 	notepicker = NotePicker()
 	notepicker.read('../Music/c1.wav')
+	notepicker.getFrequency()
+	print notepicker.frequency
 '''	audio = []
 	for index in range(1, len(sys.argv)):
 		audio.append(str(sys.argv[index]))

Test/read.py
@@ -6,53 +6,35 @@
 #
 # Last Updated: Oct 09 2015
 
-import os
 import sys
-import math
 import wave
-import struct
 import unittest
 sys.path.append('/home/psycho/Projects/Note_Picker/Source')
-from notepicker import NotePicker
-
-def generateNote(channels, sample_width, sample_rate, length, frequency):
-	signal = []
-
-	output = wave.open('test.wav', 'w')
-	output.setparams((channels, sample_width, sample_rate, 0, 'NONE', 'not compressed'))
-
-	for i in range(length * sample_rate):
-		value = math.sin(2 * math.pi * frequency * (i / float(sample_rate)))
-		packed = struct.pack('h', int(value * 2000.0 / 2))
-		signal.append(packed)
-
-	signal = ''.join(signal)
-	output.writeframes(signal)
-	output.close()
-
-def removeNote():
-	os.remove('test.wav')
+import _write
+import notepicker
 
 class ReadTest(unittest.TestCase):
+
+	
 	def testFileDoesNotExist(self):
-		notepicker = NotePicker()
+		np = notepicker.NotePicker()
 		files = ['', 'nonexistent']
 
 		for file in files:
 			try:
-				notepicker.read(file)
+				np.read(file)
 			except IOError:
 				pass
 			else:
 				self.fail('Expected error reading nonexistent file - ' + file)
 
 	def testUnsupportedFile(self):
-		notepicker = NotePicker()
+		np = notepicker.NotePicker()
 		files = ['read.py', '../README.md']
 		
 		for file in files:
 			try:
-				notepicker.read(file)
+				np.read(file)
 			except wave.Error:
 				pass
 			else:
@@ -60,91 +42,91 @@ class ReadTest(unittest.TestCase):
 
 	def testSupportedChannels(self):
 		file = 'test.wav'
-		notepicker = NotePicker()
+		np = notepicker.NotePicker()
 
 		try:
 			for channels in range(1, 2):
-				generateNote(channels, 1, 44100, 1, 440)
-				notepicker.read(file)
+				_write.generateNote(channels, 1, 44100, 1, 440)
+				np.read(file)
 
-				self.assertEqual(notepicker.channels, channels)
-				self.assertEqual(notepicker.sample_width, 1)
-				self.assertEqual(notepicker.sample_rate, 44100)
+				self.assertEqual(np.channels, channels)
+				self.assertEqual(np.sample_width, 1)
+				self.assertEqual(np.sample_rate, 44100)
 
-				self.assertIsNone(notepicker.note)
-				self.assertIsNone(notepicker.octave)
-				self.assertIsNone(notepicker.frequency)
+				self.assertIsNone(np.note)
+				self.assertIsNone(np.octave)
+				self.assertIsNone(np.frequency)
 
-				self.assertIsNotNone(notepicker.signal)
-				self.assertIsNotNone(notepicker.no_samples)
+				self.assertIsNotNone(np.signal)
+				self.assertIsNotNone(np.no_samples)
 
 		except:
 			self.fail('Error reading file with supported channels.')
 		finally:
-			removeNote()
+			_write.removeNote()
 
 	def testUnsupportedChannels(self):
 		file = 'test.wav'
-		notepicker = NotePicker()
+		np = notepicker.NotePicker()
 
 		try:
 			for channels in [3, sys.maxint]:
-				generateNote(channels, 1, 44100, 1, 440)
-				notepicker.read(file)
-		except ValueError:
+				_write.generateNote(channels, 1, 44100, 1, 440)
+				np.read(file)
+		except:
 			pass
 		else:
 			self.fail('Expected error reading unsupported channels.')
 		finally:
-			removeNote()
+			_write.removeNote()
 
 	def testSupportedSampleWidth(self):
 		file = 'test.wav'
-		notepicker = NotePicker()
+		np = notepicker.NotePicker()
 
 		try:
-			for sample_width in range(1, 4):
-				generateNote(1, sample_width, 44100, 1, 440)
-				notepicker.read(file)
+			for sample_width in range(1, 5):
+				_write.generateNote(1, sample_width, 44100, 1, 440)
+				np.read(file)
+				self.assertEqual(np.channels, 1)
+				self.assertEqual(np.sample_width, sample_width)
+				self.assertEqual(np.sample_rate, 44100)
 
-				self.assertEqual(notepicker.channels, 1)
-				self.assertEqual(notepicker.sample_width, sample_width)
-				self.assertEqual(notepicker.sample_rate, 44100)
+				self.assertIsNone(np.note)
+				self.assertIsNone(np.octave)
+				self.assertIsNone(np.frequency)
 
-				self.assertIsNone(notepicker.note)
-				self.assertIsNone(notepicker.octave)
-				self.assertIsNone(notepicker.frequency)
+				self.assertIsNotNone(np.signal)
+				self.assertIsNotNone(np.no_samples)
 
-				self.assertIsNotNone(notepicker.signal)
-				self.assertIsNotNone(notepicker.no_samples)
 		except:
 			self.fail('Error reading supported sample width.')
 		finally:
-			removeNote()
+			_write.removeNote()
 
 	def testSupportedSampleRate(self):
 		file = 'test.wav'
-		notepicker = NotePicker()
+		np = notepicker.NotePicker()
 
 		try:
 			for sample_rate in [8000, 11025, 22050, 32000, 44100, 48000, 64000, 88200, 96000]:
-				generateNote(1, 2, sample_rate, 1, 440)
-				notepicker.read(file)
+				_write.generateNote(1, 2, sample_rate, 1, 440)
+				np.read(file)
 
-				self.assertEqual(notepicker.channels, 1)
-                                self.assertEqual(notepicker.sample_width, 2)
-                                self.assertEqual(notepicker.sample_rate, sample_rate)
+				self.assertEqual(np.channels, 1)
+				self.assertEqual(np.sample_width, 2)
+				self.assertEqual(np.sample_rate, sample_rate)
 
-                                self.assertIsNone(notepicker.note)
-                                self.assertIsNone(notepicker.octave)
-                                self.assertIsNone(notepicker.frequency)
+				self.assertIsNone(np.note)
+				self.assertIsNone(np.octave)
+				self.assertIsNone(np.frequency)
 
-                                self.assertIsNotNone(notepicker.signal)
-                                self.assertIsNotNone(notepicker.no_samples)
+				self.assertIsNotNone(np.signal)
+				self.assertIsNotNone(np.no_samples)
 		except:
 			self.fail('Error reading supported sample rate.')
 		finally:
-			removeNote()
+			_write.removeNote()
 
 if __name__ == '__main__':
 	unittest.main()