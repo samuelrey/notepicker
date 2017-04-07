d07e0249796581538a5a64ffacea264d7e1b798d
Samuel Villavicencio <samuelrey010@gmail.com>
2017-04-07 02:00:32 -0700

    Inital commit 2.0

---
+++ src/note.py
@@ -0,0 +1,127 @@
+# Filename: note.py
+#
+# Summary: defines note class.
+#
+# Author: Samuel Villavicencio
+#
+# Last Updated Oct 17 2015
+
+import traceback  #
+
+import numpy  #
+import numpy.fft  #
+import scipy.signal  #
+
+
+class Note():
+    def __init__(self, start=0, length=0, signal=numpy.empty(0)):
+        self.frequency = 0.0
+        self.notation = ""
+        self.start = start
+        self.length = length
+        self.total_length = len(signal)
+
+    def getFrequency(self, signal, sample_rate):
+        ''' Determine the frequency of the signal. '''
+
+        # return the value if it has already been calculated.
+        if self.frequency != 0.0:
+            return self.frequency
+
+        if self.total_length != len(signal):
+            raise ValueError
+
+        try:
+            numpy.seterr(divide='ignore', invalid='ignore')
+            windowed = signal + scipy.signal.blackmanharris(len(signal))
+            f = numpy.fft.rfft(windowed)
+            i = numpy.argmax(abs(f))
+            true_i = 1 / 2.0 * (numpy.log(abs(f))[i - 1] - numpy.log(abs(f))[i + 1]) / (
+            numpy.log(abs(f))[i - 1] - 2 * numpy.log(abs(f))[i] + numpy.log(abs(f))[i + 1]) + i
+            self.frequency = sample_rate * true_i / len(windowed)
+            return self.frequency
+
+        except ValueError:
+            raise ValueError
+        except IndexError:
+            raise IndexError
+        except TypeError:
+            raise TypeError
+
+    def getNotation(self, frequency):
+        ''' Match the frequency to the musical note. '''
+
+        # return the value if it has already been calculated.
+        if len(self.notation) != 0:
+            return self.notation
+
+        # octaves represents the frequency bounds of every octave
+        # base_fr represents the individual frequencies in the 0th octave
+        # base_no represents the musical notation of the base frequencies
+        octaves = [31.7, 63.5, 127.1, 254.2, 508.6, 1017.4, 2034.0, 4068.0, 7902.0]
+        base_no = ['c', 'cs', 'd', 'ds', 'e', 'f', 'fs', 'g', 'gs', 'a', 'as', 'b']
+        base_fr = [16.35, 17.32, 18.35, 19.45, 20.60, 21.83, 23.12, 24.50, 25.96, 27.50, 29.14, 30.87]
+
+        try:
+            # get the octave of the frequency.
+            for index, value in enumerate(octaves):
+                if (frequency < value):
+                    octave = index
+                    break
+
+            # adjust the frequency.
+            adjusted = frequency / (2 ** octave)
+
+            # get the letter note of the frequency.
+            for index in range(12):
+                if (abs(base_fr[index] - adjusted) < .6):
+                    note = base_no[index]
+                    break
+
+            self.notation = note + str(octave)
+
+            return self.notation
+
+        except UnboundLocalError:
+            raise UnboundLocalError
+
+    def setTotalLength(self, signal):
+        ''' Set the total length of the signal. '''
+
+        try:
+            self.total_length = len(signal)
+        # catch all exceptions.
+        except:
+            print traceback.format_exc()
+
+    def setStart(self, start):
+        ''' Set the starting index of the note. '''
+
+        try:
+            if start >= self.total_length:
+                raise ValueError
+
+            elif start + self.length > self.total_length:
+                raise ValueError
+
+            else:
+                self.start = start
+        # catch all exceptions.
+        except:
+            print traceback.format_exc()
+
+    def setLength(self, length):
+        ''' Set the length of the note. '''
+
+        try:
+            if length > self.total_length:
+                raise ValueError
+
+            elif self.start + length > self.total_length:
+                raise ValueError
+
+            else:
+                self.length = length
+        # catch all exceptions.
+        except:
+            print traceback.format_exc()
---
+++ test/test_note.py
@@ -0,0 +1,188 @@
+# Filename: getFrequency.py
+#
+# Summary: unit tests getFrequency method.
+#
+# Author: Samuel Villavicencio
+#
+# Last Updated: Oct 09 2015
+
+import traceback
+import unittest
+import _write
+import note
+
+
+class GetFrequencyTest(unittest.TestCase):
+    def testEmptySignal(self):
+        n = note.Note()
+        _write.note(1, 2, 44100, 0, 440)
+
+        try:
+            signal, sample_rate = _write.read()
+            n.getFrequency(signal, sample_rate)
+        except:
+            _write.clean()
+            pass
+        else:
+            _write.clean()
+            self.fail()
+
+    def testMaxFrequency(self):
+        # values between sample_rate * n and sample_rate * (n + 1) are mistakenly valid.
+        n = note.Note()
+        _write.note(1, 2, 44100, 1, 22049)
+        try:
+            signal, sample_rate = _write.read()
+            n.setTotalLength(signal)
+            n.setStart(0)
+            n.setLength(len(signal))
+
+            self.assertAlmostEqual(22049, n.getFrequency(signal, sample_rate), 1)
+        except:
+            _write.clean()
+            self.fail(traceback.format_exc())
+        else:
+            _write.clean()
+            pass
+
+    def testMinFrequency(self):
+        n = note.Note()
+        _write.note(1, 2, 44100, 1, 1)
+        try:
+            signal, sample_rate = _write.read()
+            n.setTotalLength(signal)
+            n.setStart(0)
+            n.setLength(len(signal))
+
+            self.assertAlmostEqual(1, n.getFrequency(signal, sample_rate), 1)
+        except:
+            _write.clean()
+            self.fail(traceback.format_exc())
+        else:
+            _write.clean()
+            pass
+
+    def testPositiveUniformSignal(self):
+        n = note.Note()
+        _write.uniform(1, 1)
+        try:
+            signal, sample_rate = _write.read()
+            n.setTotalLength(signal)
+            n.setStart(0)
+            n.setLength(len(signal))
+            n.getFrequency(signal, sample_rate)
+        except:
+            _write.clean()
+            self.fail(traceback.format_exc())
+        else:
+            _write.clean()
+            pass
+
+    def testZeroUniformSignal(self):
+        n = note.Note()
+        _write.uniform(0, 1)
+        try:
+            signal, sample_rate = _write.read()
+            n.setTotalLength(signal)
+            n.setStart(0)
+            n.setLength(len(signal))
+            n.getFrequency(signal, sample_rate)
+        except:
+            _write.clean()
+            self.fail(traceback.format_exc())
+        else:
+            _write.clean()
+            pass
+
+    def testNegativeUniformSignal(self):
+        n = note.Note()
+        _write.uniform(-1, 1)
+        try:
+            signal, sample_rate = _write.read()
+            n.setTotalLength(signal)
+            n.setStart(0)
+            n.setLength(len(signal))
+            n.getFrequency(signal, sample_rate)
+        except:
+            _write.clean()
+            self.fail(traceback.format_exc())
+        else:
+            _write.clean()
+            pass
+
+    def testRandomSignal(self):
+        n = note.Note()
+        _write.rand(1)
+        try:
+            signal, sample_rate = _write.read()
+            n.setTotalLength(signal)
+            n.setStart(0)
+            n.setLength(len(signal))
+            n.getFrequency(signal, sample_rate)
+        except:
+            _write.clean()
+            self.fail(traceback.format_exc())
+        else:
+            _write.clean()
+            pass
+
+    def testAccuracy(self):
+        try:
+            for frequency in range(40, 22040, 2000):
+                n = note.Note()
+                _write.note(1, 2, 44100, 1, frequency)
+                signal, sample_rate = _write.read()
+                n.setTotalLength(signal)
+                n.setStart(0)
+                n.setLength(len(signal))
+                calculated = n.getFrequency(signal, sample_rate)
+                _write.clean()
+                self.assertAlmostEqual(frequency, calculated, 0)
+        except:
+            self.fail(traceback.format_exc())
+        else:
+            pass
+
+
+class GetNotationTest(unittest.TestCase):
+    def testNoMatchingFrequency(self):
+        for frequency in [7680.5]:  # find more frequencies
+            n = note.Note()
+            _write.note(1, 2, 44100, 1, frequency)
+            try:
+                signal, sample_rate = _write.read()
+                n.setTotalLength(signal)
+                n.setStart(0)
+                n.setLength(len(signal))
+
+                frequency = n.getFrequency(signal, sample_rate)
+                n.getNotation(frequency)
+            except:
+                _write.clean()
+                pass
+            else:
+                _write.clean()
+                self.fail(traceback.format_exc())
+
+    def testNoMatchingOctave(self):
+        for frequency in [8000.0]:
+            n = note.Note()
+            _write.note(1, 2, 44100, 1, frequency)
+            try:
+                signal, sample_rate = _write.read()
+                n.setTotalLength(signal)
+                n.setStart(0)
+                n.setLength(len(signal))
+
+                frequency = n.getFrequency(signal, sample_rate)
+                n.getNotation(frequency)
+            except:
+                _write.clean()
+                pass
+            else:
+                _write.clean()
+                self.fail(traceback.format_exc())
+
+
+if __name__ == '__main__':
+    unittest.main()
